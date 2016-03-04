01f8d5b6e06a3551f895dd8a10f2d2b812655f88
Nate Hefner <natehefner@Nathans-MacBook-Air.local>
2016-03-04 11:36:07 -0500

    fully functioning ten character limit with comments. Finished program

--- tenCharacterLimit.py
+++ tenCharacterLimit.py
@@ -1,16 +1,26 @@
 f = open("input.txt", "r")
+#opens file and reads in info
 
 inputText = f.readlines()
+#creates list with input contents
+#makes a list of lines
 
 f.close()
+#closes input file
 
 f = open("output.txt", "w")
+#opes output file with no content
 
-for word in inputText:
-	if len(word) > 10:
-		f.write(word[:10])
+for line in inputText:
+	#line represents each line in inputText
+	if len(line) > 10:
+		f.write(line[:10])
+		#if line is more than 10 characters, it will write only the first 10 characters in the output file
 		f.write("\n")
+		#spaces the lines out properly
 	else:
-		f.write(word)
+		f.write(line)
+		#writes the regular line if it has 10 characters or less
 
 f.close()
+#closes output file
